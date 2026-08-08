708b98face43cf00b090643b6288faceec2785cc
grrru <gruns0989@naver.com>
2026-08-08 11:09:18 +0900

    build a simple artificial neural network with go

---
+++ neural-network-go/go.mod
@@ -0,0 +1,5 @@
+module nn-go
+
+go 1.26.5
+
+require gonum.org/v1/gonum v0.17.0
--- neural-network-go/main.go
+++ neural-network-go/main.go
@@ -1,5 +1,367 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"time"
 
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+)
+
+// Network is a very simple 3 layer feedforward neural network.
+type Network struct {
+	inputs  int // number of neurons of the input layer
+	hiddens int // number of neurons of the hidden layer
+	outputs int // number of neurons of the output layer
+
+	hiddenWeights *mat.Dense
+	outputWeights *mat.Dense
+
+	learningRate float64
+}
+
+func CreateNetwork(input, hidden, output int, rate float64) Network {
+	net := Network{
+		inputs:       input,
+		hiddens:      hidden,
+		outputs:      output,
+		learningRate: rate,
+	}
+
+	// Setting v to net.inputs, net.hiddens doesn't have any meaning.
+	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
+	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
+
+	return net
+}
+
+// Predict process Forward propagation through 3 layers
+func (net Network) Predict(inputData []float64) mat.Matrix {
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+	return finalOutputs
+}
+
+// Train use forward propagation and backpropagation to training weights
+// Do not call Predict function at processing forward propagation
+// because we still need the other intermediary values.
+func (net *Network) Train(inputData []float64, targetData []float64) {
+	// forward propagation
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+
+	// final errors
+	targets := mat.NewDense(len(targetData), 1, targetData)
+	outputErrors := subtract(targets, finalOutputs) // Ek = tk - ok
+	hiddenErrors := dot(net.outputWeights.T(), outputErrors)
+
+	// backpropagation
+	net.outputWeights = add(net.outputWeights,
+		scale(net.learningRate,
+			dot(multiply(outputErrors, sigmoidPrime(finalOutputs)), hiddenOutputs.T()))).(*mat.Dense)
+
+	net.hiddenWeights = add(net.hiddenWeights,
+		scale(net.learningRate,
+			dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs)), inputs.T()))).(*mat.Dense)
+}
+
+// Activation function
+func sigmoid(r, c int, z float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*z))
+}
+
+// sigP = sig(1 - sig)
+func sigmoidPrime(m mat.Matrix) mat.Matrix {
+	r, _ := m.Dims()
+	o := make([]float64, r)
+	for i := range o {
+		o[i] = 1
+	}
+	ones := mat.NewDense(r, 1, o)
+	return multiply(m, subtract(ones, m))
+}
+
+func save(net Network) {
+	h, err := os.Create("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.MarshalBinaryTo(h)
+	}
+	o, err := os.Create("data/oweights.model")
+
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.MarshalBinaryTo(o)
+	}
+}
+
+// load a neural network from file
+func load(net *Network) {
+	h, err := os.Open("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.Reset()
+		net.hiddenWeights.UnmarshalBinaryFrom(h)
+	}
+	o, err := os.Open("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.Reset()
+		net.outputWeights.UnmarshalBinaryFrom(o)
+	}
+	return
+}
+
+func mnistTrain(net *Network) {
+	t1 := time.Now()
+
+	for epochs := range 5 {
+		fmt.Printf("\nepochs %d\n", epochs)
+		testFile, _ := os.Open("mnist_dataset/mnist_train.csv")
+		r := csv.NewReader(bufio.NewReader(testFile))
+
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+
+			inputs := make([]float64, net.inputs)
+			for i := range inputs {
+				x, _ := strconv.ParseFloat(record[i+1], 64)
+				inputs[i] = (x / 255.0 * 0.99) + 0.01
+			}
+
+			targets := make([]float64, 10)
+			for i := range targets {
+				targets[i] = 0.01
+			}
+			x, _ := strconv.Atoi(record[0])
+			targets[x] = 0.99
+
+			net.Train(inputs, targets)
+		}
+		testFile.Close()
+	}
+	elasped := time.Since(t1)
+	fmt.Printf("\nTime taken to train: %s\n", elasped)
+}
+
+func mnistPredict(net *Network) {
+	t1 := time.Now()
+	checkFile, _ := os.Open("mnist_dataset/mnist_test.csv")
+	defer checkFile.Close()
+
+	score := 0
+	r := csv.NewReader(bufio.NewReader(checkFile))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+
+		inputs := make([]float64, net.inputs)
+		for i := range inputs {
+			x, _ := strconv.ParseFloat(record[i+1], 64)
+			inputs[i] = (x / 255.0 * 0.99) + 0.01
+		}
+		outputs := net.Predict(inputs)
+		best := 0
+		highest := 0.0
+		for i := 0; i < net.outputs; i++ {
+			if outputs.At(i, 0) > highest {
+				best = i
+				highest = outputs.At(i, 0)
+			}
+		}
+		target, _ := strconv.Atoi(record[0])
+		if best == target {
+			score++
+		}
+	}
+
+	elapsed := time.Since(t1)
+	fmt.Printf("Time taken to check: %s\n", elapsed)
+	fmt.Println("score:", score)
+}
+
+func dataFromImage(filePath string) []float64 {
+	imgFile, err := os.Open(filePath)
+	defer imgFile.Close()
+
+	if err != nil {
+		fmt.Println("Cannot read file: ", err)
+	}
+	img, err := png.Decode(imgFile)
+	if err != nil {
+		fmt.Println("Cannot decode file: ", err)
+	}
+
+	bounds := img.Bounds()
+	gray := image.NewGray(bounds)
+
+	for x := range bounds.Max.X {
+		for y := range bounds.Max.Y {
+			var rgba = img.At(x, y)
+			gray.Set(x, y, rgba)
+		}
+	}
+
+	pixels := make([]float64, len(gray.Pix))
+	for i := range gray.Pix {
+		pixels[i] = (float64(255-gray.Pix[i]) / 255.0 * 0.99) + 0.01
+	}
+
+	return pixels
+}
+
+func predictFromImage(net Network, path string) int {
+	input := dataFromImage(path)
+	output := net.Predict(input)
+	best := 0
+	highest := 0.0
+	for i := range net.outputs {
+		if output.At(i, 0) > highest {
+			best = i
+			highest = output.At(i, 0)
+		}
+	}
+	return best
+}
+
+func printImage(img image.Image) {
+	var buf bytes.Buffer
+	png.Encode(&buf, img)
+	imgBase64Str := base64.StdEncoding.EncodeToString(buf.Bytes())
+	fmt.Printf("\x1b]1337;File=inline=1:%s\a\n", imgBase64Str)
+}
+
+func getImage(filePath string) image.Image {
+	imgFile, err := os.Open(filePath)
+	defer imgFile.Close()
+	if err != nil {
+		fmt.Println("Cannot read file:", err)
+	}
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		fmt.Println("Cannot decode file:", err)
+	}
+	return img
+}
+
+func main() {
+	// 784 inputs - 28 x 28 pixels
+	// 20 hiddens
+	// 10 outputs - digits 0 to 9
+	// learningRate - 0.1
+	net := CreateNetwork(784, 200, 10, 0.1)
+
+	mnist := flag.String("mnist", "", "Either train or predict to evaluate neural network")
+	file := flag.String("file", "", "File name of 28 x 28 PNG file to evaluate")
+	flag.Parse()
+
+	switch *mnist {
+	case "train":
+		mnistTrain(&net)
+		save(net)
+	case "predict":
+		load(&net)
+		mnistPredict(&net)
+	}
+
+	if *file != "" {
+		printImage(getImage(*file))
+		load(&net)
+		fmt.Println("prediction:", predictFromImage(net, *file))
+	}
+}
+
+// ===================================================
+// Matrix helpers
+// ===================================================
+
+func dot(m, n mat.Matrix) mat.Matrix {
+	r, _ := m.Dims()
+	_, c := n.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Product(m, n)
+	return o
+}
+
+func apply(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Apply(fn, m)
+	return o
+}
+
+func scale(s float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Scale(s, m)
+	return o
+}
+
+func multiply(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.MulElem(m, n)
+	return o
+}
+
+func add(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Add(m, n)
+	return o
+}
+
+func subtract(m, n mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Sub(m, n)
+	return o
+}
+
+func addScalar(i float64, m mat.Matrix) mat.Matrix {
+	r, c := m.Dims()
+	a := make([]float64, r*c)
+	for x := range r * c {
+		a[x] = i
+	}
+	n := mat.NewDense(r, c, a)
+	return add(m, n)
+}
+
+// The randomArray function uses the distuv package in Gonum to create a uniformly distributed set of values
+// between the range of -1/sqrt(v) and 1/sqrt(v) where v is the size of the from layer.
+func randomArray(size int, v float64) []float64 {
+	dist := distuv.Uniform{
+		Min: -1 / math.Sqrt(v),
+		Max: 1 / math.Sqrt(v),
+	}
+
+	data := make([]float64, size)
+	for i := range size {
+		data[i] = dist.Rand()
+	}
+
+	return data
 }
